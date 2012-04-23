90eaf1e821904b220dd8c7341484722dceca45a7
yu xiao <qqfishyx@gmail.com>
2012-04-23 13:24:57 +0800

    rebuild file

--- src/application/visitForum/ForumControlServlet.java
+++ src/application/visitForum/ForumControlServlet.java
@@ -1,16 +1,17 @@
 package application.visitForum;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import baseUse.Globalization;
 
+@WebServlet("/ForumControlServlet")
 public class ForumControlServlet extends HttpServlet {
 
 	/**
